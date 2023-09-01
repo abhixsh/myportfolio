08219c041a0e08fcdadf786bca421385fa0da916
Abishek Haththakage <aloka.abishek9@gmail.com>
2023-09-01 09:31:03 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "As a dedicated BICT undergraduate passionate about Full Stack Development, I am currently pursuing my career as a Frontend Developer and Web Developer.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "JavaScript",
+  "Python (Programming Language)",
+  "C (Programming Language)",
+  "Adobe Photoshop",
+  "HTML",
+  "GIT",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "With my proficiency in programming languages such as Python, JavaScript, HTML, CSS, and C, I have gained experience managing databases and creating responsive web applications. In addition to my technical skills, I am also well-versed in graphic design and have a keen eye for visual aesthetics. ";
 
 const About = () => {
   return (

a3b8f44c3694959594fde834497043307caa987b
Abishek Haththakage <aloka.abishek9@gmail.com>
2023-09-02 15:17:26 +0000

    fix image issue

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+// import image from "../images/design-desk.jpeg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -62,11 +62,11 @@ const Portfolio = () => {
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img
+          {/* <img
             src={image}
             style={{ height: "90%", width: "100%", objectFit: "cover" }}
             alt={imageAltText}
-          />
+          /> */}
         </div>
         <div className="container">
           {projectList.map((project) => (
